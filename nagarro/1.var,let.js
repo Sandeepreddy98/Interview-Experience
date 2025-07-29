898d1d4e6ba8c9b97da92d9465440ed2847329e3
Sandeep <g.sandeepreddy98@gmail.com>
2025-07-29 22:14:16 +0530

    feat(Nagarro): Program to demonstrate the difference b/w var,let,const.

---
+++ nagarro/1.var,let.js
@@ -0,0 +1,18 @@
+// For difference between var,let
+var a= 10;
+
+function test () {
+
+  console.log(a);
+
+  let a=100;
+
+  console.log(a);
+
+}
+ 
+console.log(a);
+ 
+test();
+
+//Guess the output
