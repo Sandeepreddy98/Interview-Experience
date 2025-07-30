a891a210eee69a712643632da057c0d46c6a6296
Sandeep <g.sandeepreddy98@gmail.com>
2025-07-30 23:32:51 +0530

    feat(Nextiva): TS interfaces.

---
+++ nextiva/r1.ts
@@ -0,0 +1,15 @@
+export type data = {
+    name : string,
+    age : myAge,
+}
+
+const userData : data ={
+    name : "sandeep",
+    age : 27
+}
+
+type myAge = number
+
+const usrData ={
+    name : "Sandeep",
+}
